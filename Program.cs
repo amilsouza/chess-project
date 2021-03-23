8a8b89dc99c4d54c8e1bcbeae2aa29c87a6c78c5
Amilton Souza <amiltonsouza099@gmail.com>
2021-03-23 08:31:33 -0300

    começo da implementação da camada tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posiçao" + P);
+
+            Console.ReadLine();
         }
     }
 }
